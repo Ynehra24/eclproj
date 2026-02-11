d4e0b6ada753a77c8d10e4e1404242416d90bc66
Orphieus <yatharthnehradpspune@gmail.com>
2026-02-11 19:34:08 +0530

    Phase 1 - Fixed the display and color of the page and centered the parts

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,20 +1,19 @@
-import Image from "next/image";
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="min-h-screen bg-slate-900 flex items-center justify-center">
-      <div className="bg-slate-800 text-white p-8 rounded-2xl shadow-xl w-full">
-        <h1 className="text-3xl font-bold mb-4">
+    <main className="min-h-screen bg-neutral-950 px-8 py-12 flex items-center justify-center">
+      <div className="bg-neutral-900 text-neutral-100 p-8 rounded-2xl shadow-xl w-full max-w-6xl mx-auto">
+        <h1 className="text-3xl font-semibold mb-4 text-center">
           Training Simulator
         </h1>
-        <p className="text-slate-300 mb-6">
+
+        <p className="text-neutral-400 mb-6 text-center">
           Generate role-based training scenarios dynamically.
         </p>
-        <button className="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg transition">
+
+        <button className="bg-orange-500 hover:bg-orange-600 px-5 py-2 rounded-lg transition transform hover:scale-105 block mx-auto font-medium">
           Generate Scenario
         </button>
       </div>
-      </main>
-    </div>
+    </main>
   );
 }
