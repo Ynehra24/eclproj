dde7cd5447c892d466f1776e225374ef7c00ce13
Orphieus <yatharthnehradpspune@gmail.com>
2026-02-27 01:50:47 +0530

    edited the API_BASE_URL for deployment

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "https://eclproj.onrender.com/api";
 
 export function getToken(): string | null {
     if (typeof window === "undefined") return null;
